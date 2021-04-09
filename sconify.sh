e13daf94ceac8a0cf2ad3af4169d09cbb3c36216
Christof Fetzer <christof.fetzer@scontain.com>
2021-04-09 12:48:26 +0200

    removed some optional arguments of sconify_image

--- sconify.sh
+++ sconify.sh
@@ -4,16 +4,9 @@ docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
             --network=host \
             registry.scontain.com:5050/sconecuratedimages/sconecli:sconify-image-latest sconify_image \
-            --name=c_session_name \
+            --name=helloWorldJava \
             --from=registry.scontain.com:5050/sconecuratedimages/iexecsgx:hello-world-java \
             --to=hello-world-java-scone \
-            --service-name=java-service \
-            --cas="52.157.211.165" \
-            --cas-debug \
-            --create-namespace \
-            --las="localhost" \
-            --cli="registry.scontain.com:5050/sconecuratedimages/sconecli:sconify-image-latest" \
-            --crosscompiler="registry.scontain.com:5050/sconecuratedimages/sconecli:sconify-image-latest" \
             --binary-fs \
             --binary="/usr/lib/jvm/java-11-openjdk/bin/java" \
             --fs-dir=/app \
