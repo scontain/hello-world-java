d479184b044f542ab1e7558db72d7028c1a57af5
vasyl_lukashuk <vas77794@gmail.com>
2021-05-07 21:16:09 +0200

    Update to 5.3.3

--- sconify.sh
+++ sconify.sh
@@ -2,16 +2,14 @@
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.2 sconify_iexec \
+            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.3 sconify_iexec \
             --name=helloWorldJava \
             --from=registry.scontain.com:5050/sconecuratedimages/iexecsgx:hello-world-java \
             --to=hello-world-java-scone \
             --binary-fs \
             --fs-dir=/app \
-            --fs-file=/etc/hosts \
-            --add-host=example.com:93.184.216.34 \
-            --network=host \
-            --nameserver=8.8.8.8 \
+            --host-path=/etc/hosts \
+            --host-path=/etc/resolv.conf \
             --binary="/usr/lib/jvm/java-11-openjdk/bin/java" \
             --heap="4G" \
             --dlopen="2" \
